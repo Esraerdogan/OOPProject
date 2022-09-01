8254bbf7d0ec99f61883ff11328d934fece10415
Esraerdogan <erdoganesra371@gmail.com>
2022-09-01 16:04:16 +0300

    OOP kodları
    OOP öğrenirken yazdığım kodlar

---
+++ OOPProject/app/src/main/java/com/erdoganesra/oopproject/Dog.java
@@ -0,0 +1,12 @@
+package com.erdoganesra.oopproject;
+
+public class Dog extends Animal{
+
+    public void test(){
+        super.sing();
+    }
+
+    public void sing(){
+        System.out.println("dog class");
+    }
+}
---
+++ OOPProject/app/src/main/java/com/erdoganesra/oopproject/HouseDecor.java
@@ -0,0 +1,9 @@
+package com.erdoganesra.oopproject;
+
+public interface HouseDecor {
+
+    //Static property & final
+    //Initialize
+
+    String roomName= "Kitchen";
+}
